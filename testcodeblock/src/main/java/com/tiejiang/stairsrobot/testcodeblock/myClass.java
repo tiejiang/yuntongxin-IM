9acf9d8c19d3c608be4d9c6f875f7f956e595a45
tiejiang <315904145@qq.com>
2018-05-21 17:38:21 +0800

    利用云通讯的IM创建的两个移动终端通信demo

---
+++ testcodeblock/src/main/java/com/tiejiang/stairsrobot/testcodeblock/myClass.java
@@ -0,0 +1,11 @@
+package com.tiejiang.stairsrobot.testcodeblock;
+
+public class myClass {
+
+    static String temp = " 11348.";
+
+    public static void main(String[] args){
+
+        System.out.printf("LENGTH= "+temp.length());
+    }
+}
